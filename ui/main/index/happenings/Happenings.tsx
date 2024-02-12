e525826ba933b320d2a5481ff749442e9dd1eb96
suhail-mohd07 <suhailkudige786@gmail.com>
2024-02-12 12:34:22 +0530

    happening cards

--- ui/main/index/happenings/Happenings.tsx
+++ ui/main/index/happenings/Happenings.tsx
@@ -4,8 +4,36 @@ import React from 'react'
 function Happenings() {
   return (
     <div className='w-full h-auto'>
-      <div className='w-full h-auto flex justify-center py-10'>
-        <Mhi/>
+      <div className='w-full h-auto flex flex-col items-center justify-center py-10'>
+        <Mhi />
+        <div className="w-full h-auto md:flex mt-20 ">
+          <div className="md:w-1/3 min-h-[50vh] border m-5 mx-10 p-2 cursor-pointer flex flex-col items-center rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <img src="/images/card-pic.jpg" className='relative w-full h-[80%] object-cover' alt="" />
+            <p className=" w-[90%] p-2 rounded-lg absolute bottom-2 font-light font-mono text-sm bg-gray-400 text-gray-800 hover:text-gray-900 transition-all duration-200">Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur sequi ex optio inventore, ipsum, quam dolor tempore hic quidem architecto, placeat quod asperiores nesciunt nam. Minima quam totam libero eveniet!</p>
+          </div>
+          <div className="md:w-1/3 min-h-[50vh] border m-5 mx-10 p-2 cursor-pointer flex flex-col items-center rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <img src="/images/card-pic.jpg" className='relative w-full h-[80%] object-cover' alt="" />
+            <p className=" w-[90%] p-2 rounded-lg absolute bottom-2 font-light font-mono text-sm bg-gray-400 text-gray-800hover:text-gray-900 transition-all duration-200">Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur sequi ex optio inventore, ipsum, quam dolor tempore hic quidem architecto, placeat quod asperiores nesciunt nam. Minima quam totam libero eveniet!</p>
+          </div>
+          <div className="md:w-1/3 min-h-[50vh] border m-5 mx-10 p-2 cursor-pointer flex flex-col items-center rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <img src="/images/card-pic.jpg" className='relative w-full h-[80%] object-cover' alt="" />
+            <p className=" w-[90%] p-2 rounded-lg absolute bottom-2 font-light font-mono text-sm bg-gray-400 text-gray-800hover:text-gray-900 transition-all duration-200">Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur sequi ex optio inventore, ipsum, quam dolor tempore hic quidem architecto, placeat quod asperiores nesciunt nam. Minima quam totam libero eveniet!</p>
+          </div>
+        </div>
+        <div className="w-full h-auto md:flex ">
+          <div className="md:w-1/3 min-h-[50vh] border m-5 mx-10 p-2 cursor-pointer flex flex-col items-center rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <img src="/images/card-pic.jpg" className='relative w-full h-[80%] object-cover' alt="" />
+            <p className=" w-[90%] p-2 rounded-lg absolute bottom-2 font-light font-mono text-sm bg-gray-400 text-gray-800 hover:text-gray-900 transition-all duration-200">Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur sequi ex optio inventore, ipsum, quam dolor tempore hic quidem architecto, placeat quod asperiores nesciunt nam. Minima quam totam libero eveniet!</p>
+          </div>
+          <div className="md:w-1/3 min-h-[50vh] border m-5 mx-10 p-2 cursor-pointer flex flex-col items-center rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <img src="/images/card-pic.jpg" className='relative w-full h-[80%] object-cover' alt="" />
+            <p className=" w-[90%] p-2 rounded-lg absolute bottom-2 font-light font-mono text-sm bg-gray-400 text-gray-800hover:text-gray-900 transition-all duration-200">Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur sequi ex optio inventore, ipsum, quam dolor tempore hic quidem architecto, placeat quod asperiores nesciunt nam. Minima quam totam libero eveniet!</p>
+          </div>
+          <div className="md:w-1/3 min-h-[50vh] border m-5 mx-10 p-2 cursor-pointer flex flex-col items-center rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <img src="/images/card-pic.jpg" className='relative w-full h-[80%] object-cover' alt="" />
+            <p className=" w-[90%] p-2 rounded-lg absolute bottom-2 font-light font-mono text-sm bg-gray-400 text-gray-800hover:text-gray-900 transition-all duration-200">Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur sequi ex optio inventore, ipsum, quam dolor tempore hic quidem architecto, placeat quod asperiores nesciunt nam. Minima quam totam libero eveniet!</p>
+          </div>
+        </div>
       </div>
     </div>
   )
